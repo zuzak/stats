c8951cbc98ebeef1c98c6452047b7c808f65c7e4
Douglas Gardner <douglas@chippy.ch>
2012-11-24 21:54:35 +0000

    init

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Stats</title>
+	<link href="grid.css"   type="text/css" rel="stylesheet" media="screen" />
+	<link href="styles.css" type="text/css" rel="stylesheet" media="screen" />
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+</head>
+<body>
+	<div class="row">
+		<div class="column grid_12 borded">
+			<h1>STATS!!!</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="column grid_2 borded">
+			<h2>Reddit karma</h2>
+			<p class="stat">000 &middot; 000</p>
+		</div>
+		<div class="column grid_2 borded">
+			<h2>Time at Uni</h2>
+			<p class="stat">000 days</p>
+		</div>
+		<div class="column grid_2 borded">
+			<h2>Nethack games played</h2>
+			<p class="stat">000</p>
+		</div>
+		<div class="column grid_2 borded">
+			<h2>Nethack highscore</h2>
+			<p class="stat">0000</p>
+		</div>
+		<div class="column grid_1 borded">
+			<h2>IRC pings</h2>
+			<p class="stat">0000</p>
+		</div>
+		<div class="column grid_3 borded">
+			<h2>Gross national debt</h2>
+			<p class="stat">&pound;000,000,000,000</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="column grid_1 borded">
+			<h2>IRC log size</h2>
+			<p class="stat">000MB</p>
+		</div>
+		<div class="column grid_2 borded">
+			<h2>IRC status</h2>
+			<p class="stat">Online</p>
+		</div>
+		<div class="column grid_1 borded">
+			<h2>Weather</h2>
+			<p class="stat">Rain</p>
+		</div>
+		<div class="column grid_2 borded">
+			<h2>Current Browser</h2>
+			<p class="stat">Firefox</p>
+		</div>
+		<div class="column grid_2 borded">
+			<h2>Current OS</h2>
+			<p class="stat">Arch Linux</p>
+		</div>
+		<div class="column grid_2 borded">
+			<h2>Planets with life</h2>
+			<p class="stat">0000001</p>
+		</div>
+		<div class="column grid_2 borded">
+			<h2>Steam profile</h2>
+			<p class="stat">lorem ipsum</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="column grid_2 borded">
+			<h2>University</h2>
+			<p class="stat">Aberystwyth</p>
+		</div>
+		<div class="column grid_6 borded">
+			<h2>Course</h2>
+			<p class="stat">Lorem ipsum dolor sit amet and so on and so forth</p>
+		</div>
+		<div class="column grid_3 borded">
+			<h2>Email</h2>
+			<p class="stat">xxxxxxx@xxxxxx.xxx</p>
+		</div>
+		<div class="column grid_1 borded">
+			<h2>IRC lines</h2>
+			<p class="stat">00</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="column grid_3 borded">
+			<h2>Facebook</h2>
+			<p class="stat">fb.me/abcdefghijk</p>
+		</div>
+		<div class="column grid_2 borded">
+			<h2>Number of Tweets</h2>
+			<p class="stat">0000</p>
+		</div>
+		<div class="column grid_2 borded">
+			<h2>Days since haircut</h2>
+			<p class="stat">00 days</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="column grid_12" id="footer">
+			<p>(&copy; zuzak <?php echo date("Y"); ?> &middot; <a href="http://opensource.org/licenses/BSD-3-Clause">3 Clause BSD</a>)</p>
+		</div>
+	</div>
+</body>
+</html>
